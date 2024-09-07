73d8f95ad170400e747062aaec81ae38a5ca48d2
Vicente Jorquera <jorquerav07@gmail.com>
2024-09-07 12:21:52 -0400

    feat/se agrega redux provider a la sección de cuentas

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@/auth"
 import { SessionProvider } from "next-auth/react"
+import { store } from "../redux/store"
+import { Provider } from "react-redux"
 
 export default async function ConfigurationAccountLayout(
   { children }: { children: React.ReactNode }
@@ -8,7 +10,9 @@ export default async function ConfigurationAccountLayout(
 
   return (
     <SessionProvider session={session}>
-      {children}
-    </SessionProvider>
+      <Provider store={store}>
+        {children}
+      </Provider>
+    </SessionProvider >
   )
 }
