20aaefdb4bed1d3a8c61ad0c3bd20b6a29f89f42
Vicente Jorquera <jorquerav07@gmail.com>
2024-09-07 12:51:18 -0400

    feat/se hace uso del nuevo provider en la sección layout de cuentas

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth"
+import ReduxProvider from "@/providers/ReduxProvider"
 import { SessionProvider } from "next-auth/react"
-import { store } from "../../redux/store"
-import { Provider } from "react-redux"
+
 
 export default async function ConfigurationAccountLayout(
   { children }: { children: React.ReactNode }
@@ -10,9 +10,9 @@ export default async function ConfigurationAccountLayout(
 
   return (
     <SessionProvider session={session}>
-      <Provider store={store}>
+      <ReduxProvider>
         {children}
-      </Provider>
+      </ReduxProvider>
     </SessionProvider >
   )
 }
