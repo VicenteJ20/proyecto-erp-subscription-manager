0e0bae181b0c19f1c8b5289f39657769286fbcff
Vicente Jorquera <jorquerav07@gmail.com>
2024-09-07 20:52:04 -0400

    feat/se modifica el funcionamiento del componente para gatillar formularios en base a su id

--- src/components/config/ChangeBtn.tsx
+++ src/components/config/ChangeBtn.tsx
@@ -2,24 +2,26 @@
 
 import { Button } from "../ui/button"
 import { useSelector, useDispatch } from "react-redux"
-import { decrement, increment } from "@/redux/features/account/stageSlice"
+import { decrement } from "@/redux/features/account/stageSlice"
+import { handleExternalSubmit } from "@/utils/formUtils"
 
-const ChangeBtn = () => {
+const ChangeBtn = ({id}: { id: string}) => {
   const currentStage = useSelector((state: any) => state.currentStage.stage)
   const dispatch = useDispatch()
-
-  const handleNext = () => {
-    dispatch(increment())
-  }
 
   const handleBack = () => {
     dispatch(decrement())
   }
 
+
   return (
-    <section>
-      <Button onClick={handleNext} className='bg-zinc-500 text-white'>Avanzar</Button>
-      <Button onClick={handleBack} className='bg-zinc-500 text-white'>Volver</Button>
+    <section className='w-full px-12 py-6 gap-2 flex flex-row-reverse'>
+      <Button onClick={() => handleExternalSubmit(id)} size='lg' className='px-4 py-4 bg-teal-600 text-white border border-teal-500 text-base hover:text-white'>
+        {currentStage === 4 ? 'Confirmar' : 'Siguiente'}
+      </Button>
+      <Button size='lg' className='px-4 py-4 bg-white text-black border border-zinc-100 text-base hover:text-white'>
+        Volver
+      </Button>
     </section>
   )
 }
