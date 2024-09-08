2f3eeb721a59eb4f13621b0031595be69b9e77d1
Vicente Jorquera <jorquerav07@gmail.com>
2024-09-07 20:58:58 -0400

    feat/se configura nuevos componentes

--- src/app/account/config/page.tsx
+++ src/app/account/config/page.tsx
@@ -1,6 +1,14 @@
-const ConfigurationPage = () => {
+import { HeaderStepAccount } from "@/components/config/Header"
+import { ManagerForm } from "@/components/config/ManagerForm"
+import { auth } from "@/auth"
+
+const ConfigurationPage = async () => {
+  const session = await auth()
   return (
-    <h2>PÁGINA DE CONFIGURACIÓN DE CUENTA</h2>
+    <section className='max-h-full py-12 flex flex-col gap-6'>
+      <HeaderStepAccount title='Información personal' description='Te damos la bienvenida a EPYME, necesitamos más información para continuar con la configuración de su cuenta.' />
+      <ManagerForm email={session?.user?.email || '' }/>
+    </section>
   )
 }
 
