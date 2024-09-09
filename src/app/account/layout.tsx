7a8b1b5788a21cfe7e53712ba3af57b18de37cd6
Vicente Jorquera <jorquerav07@gmail.com>
2024-09-09 01:25:24 -0300

    feat/se actualiza layout de vistas de cuentas

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -2,23 +2,22 @@ import { auth } from "@/auth"
 import ReduxProvider from "@/providers/ReduxProvider"
 import { SessionProvider } from "next-auth/react"
 import { SideBarStage } from "@/components/config/SideBarStage"
-import { ChangeBtn } from "@/components/config/ChangeBtn"
+import { AccountFooterButtons } from "@/components/config/AccountFooterButtons"
 
 export default async function ConfigurationAccountLayout(
-  { children }: { children: React.ReactNode }
+  { children }: { children: React.ReactNode
+  }
 ) {
   const session = await auth()
-
+  
   return (
     <SessionProvider session={session}>
       <ReduxProvider>
-        <section className='grid grid-cols-1 pl-[30rem] h-screen relative'>
+        <section className='grid grid-cols-1 pl-[30rem] min-h-screen relative'>
           <SideBarStage />
-          <section className='px-12 w-full'>
+          <section className='px-12 w-full pb-12'>
             {children}
-            <section className='absolute bottom-0 left-[30rem] right-0'>
-              <ChangeBtn id='managerForm' />
-            </section>
+            <AccountFooterButtons />
           </section>
         </section>
       </ReduxProvider>
